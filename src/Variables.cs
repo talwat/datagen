6cfe1a3ddb8234223f3d614a2e75df42ef55a211
Tal <talrac1020@gmail.com>
2021-11-06 14:08:20 +0100

    Updated version number.

--- src/Variables.cs
+++ src/Variables.cs
@@ -9,7 +9,7 @@ namespace Variables {
         //Variable for the raw input.
         public static string rawInput;
         //Universal Version Variable.
-        public static string version = "0.5.1 DEV";
+        public static string version = "0.5.3 DEV";
         //Core variable to tell the program if it is runned by the terminal or by normal means.
         public static bool core;
     }
